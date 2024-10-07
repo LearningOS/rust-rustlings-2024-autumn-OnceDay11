9d129943a2f493b29c2a8357d2b07c0d0131ae5a
Once Day <onceday@qq.com>
2024-10-07 23:42:36 +0800

    完成80-97道题目练习
    Signed-off-by: Once Day <once_day@qq.com>

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let ss: Vec<&str> = s.split(',').collect();
+        if ss.len() != 2 {
+            Person::default()
+        } else if ss[0].len() == 0 || ss[1].len() == 0 {
+            Person::default()
+        } else {
+            let age = match ss[1].parse::<usize>() {
+                Ok(v) => v,
+                Err(_) => return Person::default()
+            };
+            Person {name: ss[0].to_string(), age: age}
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let ss: Vec<&str> = s.split(',').collect();
+        if ss.len() != 2 {
+            Err(ParsePersonError::BadLen)
+        } else if ss[0].len() == 0 {
+            Err(ParsePersonError::NoName)
+        } else {
+            let age = match ss[1].parse::<usize>() {
+                Ok(v) => v,
+                Err(e) => return Err(ParsePersonError::ParseInt(e)),
+            };
+            Ok(Person {
+                name: ss[0].to_string(),
+                age: age,
+            })
+        }
     }
 }
 
