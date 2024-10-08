3320d8335d5a4f957c7bfe3d297a8b57ad597c4e
Once Day <onceday@qq.com>
2024-10-08 18:20:45 +0800

    完成剩余的Tests题目
    Signed-off-by: Once Day <once_day@qq.com>

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -34,13 +34,13 @@
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn main() {}
+fn main() {
+    println!("timestamp: {:?}", std::env::var("TEST_FOO"));
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    //use super::*;
 
     #[test]
     fn test_success() {
@@ -48,7 +48,7 @@ mod tests {
             .duration_since(std::time::UNIX_EPOCH)
             .unwrap()
             .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap();
+        let s: String = std::env::var("TEST_FOO").unwrap();
         let e: u64 = s.parse().unwrap();
         assert!(timestamp >= e && timestamp < e + 10);
     }
